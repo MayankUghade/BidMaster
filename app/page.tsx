cf15caf691e79e2f20ad71599bbec8e3d69a9091
Mayank Ughade <mayank14ughade@gmail.com>
2024-06-22 17:28:07 +0530

    Adding login and signup functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import { SignIn } from "./sign-up";
 
 export default function Home() {
-  return <div>This is a next js application</div>;
+  return (
+    <div>
+      This is a next js application
+      <SignIn />
+    </div>
+  );
 }
